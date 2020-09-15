14832f6cb06f6eec32e29ac30ab1f64599a9ea00
Mersad Hj <mersadhassanjani@gmail.com>
2020-09-15 18:19:46 +0430

    Updates

---
+++ precache-manifest.aaeeae9116be25f287de31c619994ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a05eefd67e11f733e30ffa15ebcd6e",
+    "url": "/search-movies/index.html"
+  },
+  {
+    "revision": "a0ace9b3c04fa9574451",
+    "url": "/search-movies/static/css/main.2d074e19.chunk.css"
+  },
+  {
+    "revision": "72088f4afb918055bb9f",
+    "url": "/search-movies/static/js/2.d4efdb1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-movies/static/js/2.d4efdb1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ace9b3c04fa9574451",
+    "url": "/search-movies/static/js/main.7169247b.chunk.js"
+  },
+  {
+    "revision": "5f2b78dd8d4d9038b6cd",
+    "url": "/search-movies/static/js/runtime-main.28acf4f8.js"
+  }
+]);
